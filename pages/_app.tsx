10b66eba3bfc0f3ad5f2e45eae08a93ea4debbd1
lehmannerich <lehmannerich@live.de>
2023-04-04 19:00:42 +0200

    Added title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 import "@vercel/examples-ui/globals.css";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Next.js Chatbot</title>
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
